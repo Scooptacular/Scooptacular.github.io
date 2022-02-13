47906ce7a75bbd59847e6a6db511604715cebaea
Hamish Lester <hamishlester555@gmail.com>
2022-02-14 12:03:14 +1300

    Update

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-git push -u origin master
 git add .
 git status
+git push -u origin master
 git commit -m 'Update'
 git remote add origin 'https://github.com/Scooptacular/scooptacular.github.io.git'
 git push -u origin master
